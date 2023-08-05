a3a39c88a49344d820c0b8a3d2e475d4b167641a
anys34 <anyeseong34@gmail.com>
2023-08-05 18:38:14 +0900

    ADD :: 전체 데이터 출력

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const People_Info = require("../models/people_info");
+
+// 데이터 전체 출력
+router.get("/", async (req, res) => {
+  const data = await People_Info.findAll({});
+  res.send(data);
+});
 
 module.exports = router;
