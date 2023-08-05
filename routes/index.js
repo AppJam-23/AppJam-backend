ba40bb50c34dfb98a9b3e7677d9a5c08266abd1e
anys34 <anyeseong34@gmail.com>
2023-08-05 18:42:57 +0900

    ADD :: 환자 전체 출력

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,26 @@ const router = express.Router();
 const People_Info = require("../models/people_info");
 
 // 데이터 전체 출력
-router.get("/", async (req, res) => {
+router.get("/", async(req, res) => {
   const data = await People_Info.findAll({});
   res.send(data);
 });
 
-// 의심 질환자 출력
+// 의심 질환자 전체 출력
 router.get('/suspected', async(req, res) => {
     const data = await People_Info.findAll({
         where: {
             Suspected: true,
+        }
+    });
+    res.send(data);
+});
+
+// 환자 전체 출력
+router.get('/patient', async(req, res) => {
+    const data = await People_Info.findAll({
+        where: {
+            Patient: true,
         }
     });
     res.send(data);
