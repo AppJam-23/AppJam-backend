40efca327e94769942c9af9d9f87946b5801b816
anys34 <anyeseong34@gmail.com>
2023-08-05 18:51:03 +0900

    FIX :: 포트 번호

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { sequelize } = require("./models");
 const indexRouter = require("./routes/index");
 
 const app = express();
-app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 3000);
 app.set("view engine", "html");
 nunjucks.configure("views", {
   express: app,
